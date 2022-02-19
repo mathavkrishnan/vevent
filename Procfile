1c1a9af4f6a68bd9a06aae3b5cfa6abbe15cfa08
Mathavkrishna <mathavkrishnan.s2020@vitstudent.ac.in>
2022-02-19 18:38:12 +0530

    Veventz

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn veventz.wsgi --log-file -
+release: python manage.py migrate
+web: gunicorn veventz.wsgi
--- veventz/settings.py
+++ veventz/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,10 +85,7 @@ WSGI_APPLICATION = 'veventz.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config()
 }
 
 AUTHENTICATION_BACKENDS = [
@@ -141,3 +139,4 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
